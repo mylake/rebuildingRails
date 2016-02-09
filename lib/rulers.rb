4bf1886857dd279714332e24e400a34f6f27bdb8
Lake Chang <lake.chang@vpon.com>
2016-02-09 20:06:02 +0800

    routing

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,13 +1,29 @@
+require 'pry'
+
 require 'rulers/version'
 require 'rulers/array'
+require 'rulers/routing'
 
 module Rulers
   class Application
     def call(env)
-      `echo debug > debug.txt`;
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
 
-      [200, {'Content-Type' => 'text/html'},
-        ['Hello from Ruby on Rulers!']]
+      [200, {"Content-Type" => "text/html"},
+        [text]]
+    end
+  end
+
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
--- rulers.gemspec
+++ rulers.gemspec
@@ -24,5 +24,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rspec", "~> 3.0"
 
   spec.add_runtime_dependency "rest-client", '~> 0'
-  spec.add_runtime_dependency "rack", '~> 0'
+  spec.add_runtime_dependency "rack", '~> 1.6'
 end
