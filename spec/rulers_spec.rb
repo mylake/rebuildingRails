83d5b51dcded8ed588eedff2e74b6663478717c0
Lake Chang <lake.chang@vpon.com>
2016-02-12 07:50:08 +0800

    use rspec-given as test framework

--- spec/rulers_spec.rb
+++ spec/rulers_spec.rb
@@ -1,11 +1,9 @@
 require 'spec_helper'
 
 describe Rulers do
-  it 'has a version number' do
-    expect(Rulers::VERSION).not_to be nil
+  context 'has a version number' do
+    Given(:version) { Rulers::VERSION }
+    Then { version != nil}
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
